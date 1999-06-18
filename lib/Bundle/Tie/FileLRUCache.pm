fc3bfaeb7653df62db9fa9dca75dcd8bfa0381ea
Benjamin Franz <snowhare@nihongo.org>
1999-06-18 15:34:07 +0000

    Import of SNOWHARE/Bundle-Tie-FileLRUCache-1.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Tie-FileLRUCache
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         SNOWHARE/Bundle-Tie-FileLRUCache-1.01.tar.gz
    gitpan-cpan-author:       SNOWHARE
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Tie/FileLRUCache.pm
@@ -0,0 +1,36 @@
+package Bundle::Tie::FileLRUCache;
+
+$VERSION = '1.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Tie::FileLRUCache - A bundle to install all Tie::FileLRUCache related modules
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::Tie::FileLRUCache'>
+
+=head1 CONTENTS
+
+Digest::MD5             - For Tie::DB_File::SplitHash and Tie::FileLRUCache
+
+Class::NamedParms       - Used by most of the system
+
+Class::ParmList         - Used by most of the system
+
+Tie::FileLRUCache       - The Tie::FileLRUCache module itself 
+
+=head1 DESCRIPTION
+
+This bundle includes the Tie::FileLRUCache module and all modules
+that it depends on.
+
+=head1 AUTHOR
+
+Benjamin Franz E<lt>F<snowhare@nihongo.org>E<gt>
+
+=cut
